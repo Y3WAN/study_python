d905f5ef140a1435f799a3f817b4d2b1980677fc
Y3WAN <ywyw2848@gmail.com>
2022-01-09 19:49:12 +0900

    for, while

--- 가위바위보.py
+++ 가위바위보.py
@@ -1,25 +1,25 @@
 import random
-a=input("입력: ")
-b=random.randint(1,3)
-if a=="가위"and b==1:
+a = input("입력: ")
+b = random.randint(1,3)
+if a == "가위"and b == 1:
     print("같음")
-elif a=="가위" and b==2:
+elif a == "가위" and b == 2:
     print("짐")
-elif a=="가위" and b==3:
+elif a == "가위" and b == 3:
     print("이김")
 
-elif a=="바위"and b==2:
+elif a == "바위"and b == 2:
     print("같음")
-elif a=="바위" and b==3:
+elif a == "바위" and b == 3:
     print("짐")
-elif a=="바위" and b==1:
+elif a == "바위" and b == 1:
     print("이김")
     
-if a=="보"and b==3:
+if a == "보"and b == 3:
     print("같음")
-elif a=="보" and b==1:
+elif a == "보" and b == 1:
     print("짐")
-elif a=="보" and b==2:
+elif a == "보" and b == 2:
     print("이김")
 
 
